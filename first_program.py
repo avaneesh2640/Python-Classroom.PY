2509b61154162f1d45e76580315159810025fc24
avaneesh@123 <avaneeshsharma9219@gmail.com>
2026-02-03 20:56:42 +0530

    Add files via upload

---
+++ first_program.py
@@ -0,0 +1,43 @@
+# i = 1
+# while i <= 100:
+#     print("give me a good solution")
+#     i += 1
+#     if i == 100:
+#         print("yes")  
+
+
+# for i in range(1, 100) :
+#     print(i)
+
+
+#n =int(input("enter a number: "))
+
+# for i in range(1, 11) :
+#     print(f"{n} x {i} = {n*i}")
+
+# n = 6
+# fact = 1
+# for i in range(1, n+1) :
+#     fact *= i   
+#     print(f"fact =" , fact)
+
+# def calcs_multiply(a, b):
+#     multiply = a * b
+#     print(multiply)
+#     return multiply
+
+# # call function 1st time
+# calcs_multiply(2, 2)
+
+# # call function 2nd time
+# calcs_multiply(3, 4)
+
+# # call function 3rd time
+# calcs_multiply(5,6)
+
+cities = ["New York", "Los Angeles", "Chicago", "Houston", "Phoenix"]
+
+def print_list(lst):
+    print(len(lst))
+
+print_list(cities)
